6eb3d33fa037ebe6d01820233faf99b6fea59110
juttayaya <ec2-user@ip-172-31-71-197.ec2.internal>
2018-11-02 04:00:24 +0000

    add dynamo db for locking

--- terraform/tf-infra/tf-s3-dynamo.tf
+++ terraform/tf-infra/tf-s3-dynamo.tf
@@ -10,6 +10,10 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+locals {
+    project_name  = "JavaJirawat EKS demo"
+}
  
 resource "aws_s3_bucket" "tfstate-s3-eks" {
     bucket = "tfstate-s3-eks"
@@ -25,6 +29,23 @@ resource "aws_s3_bucket" "tfstate-s3-eks" {
  
     tags {
       Name = "Terraform tfstate S3 for EKS demo"
-      Org = "JavaJirawat EKS demo"
+      Org = "${local.project_name}"
     }
 }      
+
+resource "aws_dynamodb_table" "tflock-eks" {
+  name = "tflock-eks"
+  hash_key = "LockID"
+  read_capacity = 5
+  write_capacity = 5
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+ 
+  tags {
+    Name = "Terraform DynamoDB Lock Table for EKS demo"
+    Org = "${local.project_name}"
+  }
+}
